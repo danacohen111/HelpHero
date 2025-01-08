6599e4ec22dddee6d86ee003158ba591d5b01c23
danacohen111 <danac2121@gmail.com>
2025-01-08 15:03:06 +0200

    create navigation setup

--- app/src/main/java/com/example/helphero/MainActivity.kt
+++ app/src/main/java/com/example/helphero/MainActivity.kt
@@ -1,40 +1,26 @@
-package com.example.helphero
+package com.yourpackage
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.helphero.R
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var emailField: EditText
-    private lateinit var passwordField: EditText
-    private lateinit var continueButton: Button
-    private lateinit var signupButton: Button
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        emailField = findViewById(R.id.emailField)
-        passwordField = findViewById(R.id.passwordField)
-        continueButton = findViewById(R.id.continueButton)
-        signupButton = findViewById(R.id.signupButton)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
-        continueButton.setOnClickListener {
-            val email = emailField.text.toString().trim()
-            val password = passwordField.text.toString().trim()
-
-            // Basic validation (check if fields are empty)
-            if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "Please fill out both fields", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        signupButton.setOnClickListener {
-            finish()
-        }
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        NavigationUI.setupWithNavController(topAppBar, navController)
     }
 }
-

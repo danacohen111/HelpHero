6427922583acaef66650d8b4c0ba57f957a2fbe1
Ilana Belokon <ilana.belokon2@cs.colman.ac.il>
2025-01-08 15:03:20 +0200

    add basic implementation for signup page  - without auth

--- app/src/main/java/com/example/helphero/ui/signup/SignInViewModel.kt
+++ app/src/main/java/com/example/helphero/ui/signup/SignInViewModel.kt
@@ -1,23 +1,23 @@
 package com.yourpackage.ui.signup
 
 import androidx.lifecycle.ViewModel
-import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.auth.FirebaseAuth
 
 class SignUpViewModel : ViewModel() {
 
-    fun signUp(
-        auth: FirebaseAuth,
-        email: String,
-        password: String,
-        onComplete: (isSuccessful: Boolean, errorMessage: String?) -> Unit
-    ) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    onComplete(true, null)
-                } else {
-                    onComplete(false, task.exception?.message)
-                }
-            }
-    }
+//    fun signUp(
+//        auth: FirebaseAuth,
+//        email: String,
+//        password: String,
+//        onComplete: (isSuccessful: Boolean, errorMessage: String?) -> Unit
+//    ) {
+//        auth.createUserWithEmailAndPassword(email, password)
+//            .addOnCompleteListener { task ->
+//                if (task.isSuccessful) {
+//                    onComplete(true, null)
+//                } else {
+//                    onComplete(false, task.exception?.message)
+//                }
+//            }
+//    }
 }

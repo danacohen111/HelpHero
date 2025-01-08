3c6b939ea62aa3ca37d6527ecffeaecf83fbdba8
danacohen111 <danac2121@gmail.com>
2025-01-08 17:02:35 +0200

    create models

---
+++ app/src/androidTest/java/com/example/helphero/models/Post.kt
@@ -0,0 +1,9 @@
+package com.example.helphero.models
+
+data class Post(
+    val postId: String,
+    val userId: String, // Reference to the user who made the post
+    val imageUrl: String,
+    val date: String,
+    val comments: List<Comment>
+)

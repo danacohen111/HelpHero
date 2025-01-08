20e1702331efb72a123f1de60ccbfaacb80e29af
Ilana Belokon <ilana.belokon2@cs.colman.ac.il>
2025-01-08 17:59:41 +0200

    add missing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,15 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+}

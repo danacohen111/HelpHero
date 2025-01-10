299fdf855aae2f13a80966f0da2afdd24c5da346
danacohen111 <danac2121@gmail.com>
2025-01-10 13:30:47 +0200

    fix comment model

--- app/src/main/java/com/example/helphero/models/Comment.kt
+++ app/src/main/java/com/example/helphero/models/Comment.kt
@@ -7,11 +7,13 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "comments")
 data class Comment (
     @PrimaryKey var commentId: String,
+    @ColumnInfo(name = "postId") var postId: String,
     @ColumnInfo(name = "userId") var userId: String,
     @ColumnInfo(name = "text") var text: String
 )
 
 data class FirestoreComment(
+    val postId: String = "",
     val userId: String = "",
     val text: String = ""
 )
@@ -19,6 +21,7 @@ data class FirestoreComment(
 fun FirestoreComment.toRoomComment(commentId: String): Comment {
     return Comment(
         commentId = commentId,
+        postId = this.postId,
         userId = this.userId,
         text = this.text
     )
@@ -26,6 +29,7 @@ fun FirestoreComment.toRoomComment(commentId: String): Comment {
 
 fun Comment.toFirestoreComment(): FirestoreComment {
     return FirestoreComment(
+        postId = this.postId,
         userId = this.userId,
         text = this.text
     )

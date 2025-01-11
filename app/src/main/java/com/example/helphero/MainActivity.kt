4f4e9a94827e65fa6f4130e9004e2e74c0f8acf5
danacohen111 <danac2121@gmail.com>
2025-01-11 12:05:59 +0200

    add menu

--- app/src/main/java/com/example/helphero/MainActivity.kt
+++ app/src/main/java/com/example/helphero/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.yourpackage
+package com.example.helphero
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +7,8 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.helphero.R
 import com.google.android.material.appbar.MaterialToolbar
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,5 +24,53 @@ class MainActivity : AppCompatActivity() {
 
         val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
         NavigationUI.setupWithNavController(topAppBar, navController)
+
+        topAppBar.setNavigationOnClickListener {
+            navController.navigateUp()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_top_navigation, menu)
+
+        for (i in 0 until menu?.size()!!) {
+            val item = menu.getItem(i)
+            item.isChecked = false
+        }
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.homeFragment -> {
+                resetMenuItems(findViewById<MaterialToolbar>(R.id.topAppBar).menu)
+                item.isChecked = true
+                navController.navigate(R.id.homeFragment)
+                return true
+            }
+            R.id.profileFragment -> {
+                resetMenuItems(findViewById<MaterialToolbar>(R.id.topAppBar).menu)
+                item.isChecked = true
+                navController.navigate(R.id.profileFragment)
+                return true
+            }
+            R.id.addPostFragment -> {
+                resetMenuItems(findViewById<MaterialToolbar>(R.id.topAppBar).menu)
+                item.isChecked = true
+                navController.navigate(R.id.addPostFragment)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun resetMenuItems(menu: Menu?) {
+        menu?.let {
+            for (i in 0 until it.size()) {
+                val item = it.getItem(i)
+                item.isChecked = false
+            }
+        }
     }
 }

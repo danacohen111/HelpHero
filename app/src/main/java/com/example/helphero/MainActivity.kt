47622daf287af32344cb3e307c6d194b8d201559
danacohen111 <danac2121@gmail.com>
2025-01-14 19:00:45 +0200

    add navigation to signin and make the app bar invisible when in signin

--- app/src/main/java/com/example/helphero/MainActivity.kt
+++ app/src/main/java/com/example/helphero/MainActivity.kt
@@ -9,11 +9,14 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.helphero.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var isLoggedIn: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,42 +31,57 @@ class MainActivity : AppCompatActivity() {
         // Reference the MaterialToolbar from app_bar.xml (via include in activity_main.xml)
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
-        toolbar.setTitle("");
-        getSupportActionBar()?.setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("")
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         // Set up the toolbar with NavController
         NavigationUI.setupWithNavController(toolbar, navController)
+
+        isLoggedin()
+
+        if (isLoggedIn) {
+            navController.navigate(R.id.homeFragment)
+        } else {
+            navController.navigate(R.id.signInFragment)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_top_navigation, menu)
-        Log.d("MainActivity", "inflated !!")
         return true
     }
 
-        override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-            R.id.homeFragment -> {
-                navController.navigate(R.id.homeFragment)
-                Log.d("MainActivity", "moving to home :)")
-                true
-            }
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        super.onPrepareOptionsMenu(menu)
+        menu?.findItem(R.id.homeFragment)?.isVisible = isLoggedIn
+        menu?.findItem(R.id.profileFragment)?.isVisible = isLoggedIn
+        menu?.findItem(R.id.addPostFragment)?.isVisible = isLoggedIn
+        menu?.findItem(R.id.applogo_menu)?.isVisible = isLoggedIn
+        menu?.findItem(R.id.israel_menu)?.isVisible = isLoggedIn
+        return true
+    }
 
-            R.id.profileFragment -> {
-                navController.navigate(R.id.profileFragment)
-                Log.d("MainActivity", "moving to profile :0")
-                true
-            }
-
-            R.id.addPostFragment -> {
-                navController.navigate(R.id.addPostFragment)
-                Log.d("MainActivity", "moving to add post ->")
-                true
-            }
-
-            else -> {
-                // The user's action isn't recognized.
-                // Invoke the superclass to handle it.
-                super.onOptionsItemSelected(item)
-            }
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.homeFragment -> {
+            navController.navigate(R.id.homeFragment)
+            true
         }
+        R.id.profileFragment -> {
+            navController.navigate(R.id.profileFragment)
+            true
+        }
+        R.id.addPostFragment -> {
+            navController.navigate(R.id.addPostFragment)
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    fun isLoggedin() {
+        invalidateOptionsMenu()
+        if (auth.currentUser != null)
+            isLoggedIn = true
+        else
+            isLoggedIn = false
+    }
 }
--- app/src/main/java/com/example/helphero/ui/signin/signInFragment.kt
+++ app/src/main/java/com/example/helphero/ui/signin/signInFragment.kt
@@ -1,12 +1,35 @@
 package com.example.helphero.ui.signin
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.helphero.R
+import com.example.helphero.databinding.FragmentSigninBinding
 
-class SignInFragment : Fragment(R.layout.fragment_signin) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // TODO: Implement the ViewModel and the logic for the SignInFragment
+class SignInFragment : Fragment() {
+    //TODO: Only created for navigation test purpose. Remove later
+    private var _binding: FragmentSigninBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSigninBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSignIn.setOnClickListener {
+            // Handle sign-in logic here
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

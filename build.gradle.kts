e9c51c4e098ccd6512fd31c581247c51d5924bc5
Ilana Belokon <ilana.belokon2@cs.colman.ac.il>
2025-01-22 15:06:50 +0200

    handle conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.version
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+        classpath(libs.kotlin.gradle.plugin)
+    }
 }

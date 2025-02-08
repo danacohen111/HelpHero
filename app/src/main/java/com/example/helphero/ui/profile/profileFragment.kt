c04a9ab8cc670c624b17c7eb9dfdee5c527c61ff
danacohen111 <danac2121@gmail.com>
2025-02-08 21:18:32 +0200

    fix profile

--- app/src/main/java/com/example/helphero/ui/profile/profileFragment.kt
+++ app/src/main/java/com/example/helphero/ui/profile/profileFragment.kt
@@ -1,6 +1,7 @@
 package com.example.helphero.ui.profile
 
 import android.app.AlertDialog
+import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -8,7 +9,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -19,15 +22,20 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.room.InvalidationTracker
 import com.example.helphero.R
 import com.example.helphero.databases.comments.CommentDatabase
+import com.example.helphero.databases.users.UserDatabase
 import com.example.helphero.databinding.FragmentProfileBinding
 import com.example.helphero.models.Post
+import com.example.helphero.models.User
 import com.example.helphero.repositories.CommentRepository
+import com.example.helphero.repositories.UserRepository
 import com.example.helphero.ui.adapters.PostAdapter
 import com.example.helphero.ui.viewmodels.CommentViewModel
 import com.example.helphero.ui.viewmodels.PostViewModel
+import com.example.helphero.ui.viewmodels.ProfileViewModel
+import com.example.helphero.ui.viewmodels.ProfileViewModelFactory
+import com.example.helphero.utils.ImageUtil
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.launch
@@ -42,18 +50,39 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private var selectedImageUri: Uri? = null
     private var pendingPostUpdate: Pair<String, String>? = null
 
-    private val commentRepository: CommentRepository by lazy {
+    private var isEditMode = false
+    private var currentUser: User? = null
+    private var imageUri: Uri? = null
+
+    private lateinit var commentRepository: CommentRepository
+    private lateinit var commentViewModel: CommentViewModel
+    private lateinit var userRepository: UserRepository
+    private lateinit var profileViewModel: ProfileViewModel
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
         val firestoreDb = FirebaseFirestore.getInstance()
         val firebaseAuth = FirebaseAuth.getInstance()
-        val commentDao = CommentDatabase.getDatabase(requireContext()).commentDao()
-        CommentRepository(firestoreDb, firebaseAuth, commentDao)
-    }
+        val commentDao = CommentDatabase.getDatabase(context).commentDao()
+        commentRepository = CommentRepository(firestoreDb, firebaseAuth, commentDao)
 
-    private val commentViewModel: CommentViewModel by lazy {
-        ViewModelProvider(
+        val userDao = UserDatabase.getDatabase(context).userDao()
+        userRepository = UserRepository(
+            firestoreDb = firestoreDb,
+            firestoreAuth = firebaseAuth,
+            userDao = userDao
+        )
+
+        commentViewModel = ViewModelProvider(
             requireActivity(),
             CommentViewModel.CommentViewModelFactory(commentRepository)
         )[CommentViewModel::class.java]
+
+        profileViewModel = ViewModelProvider(
+            requireActivity(),
+            ProfileViewModelFactory(userRepository)
+        )[ProfileViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -67,28 +96,104 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnSignOut.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            Toast.makeText(requireContext(), "Signed Out Successfully", Toast.LENGTH_SHORT).show()
-            findNavController().navigate(R.id.signInFragment)
-        }
+        context?.let { context ->
+            profileViewModel.user.observe(viewLifecycleOwner) { user ->
+                currentUser = user
+                binding.tvUsername.text = user.name
+                binding.tvEmail.text = user.email
+                binding.tvPhone.text = user.phone
+                binding.etPhone.setText(user.phone)
 
-        val currentUserId = FirebaseAuth.getInstance().currentUser?.uid ?: return
-        postViewModel.fetchUserPosts(currentUserId)
+                if (!user.photoUrl.isNullOrEmpty()) {
+                    ImageUtil.loadImage(
+                        Uri.parse(user.photoUrl),
+                        binding.btnProfileImage,
+                        R.drawable.ic_profile_placeholder
+                    )
+                } else {
+                    binding.btnProfileImage.setImageResource(R.drawable.ic_profile_placeholder)
+                }
+            }
 
-        val adapter = PostAdapter(
-            lifecycleOwner = viewLifecycleOwner,
-            context = requireContext(),
-            onEditClick = { post -> updatePost(post) },
-            onDeleteClick = { post -> deletePost(post) }
-        )
-        binding.recyclerViewUserPosts.adapter = adapter
-        binding.recyclerViewUserPosts.layoutManager = LinearLayoutManager(requireContext())
+            binding.btnProfileImage.setOnClickListener {
+                pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            }
 
-        postViewModel.userPosts.observe(viewLifecycleOwner) { posts ->
-            adapter.submitList(posts)
+            binding.ivEditProfile.setOnClickListener {
+                isEditMode = !isEditMode
+                toggleEditMode(isEditMode)
+            }
+
+            binding.btnSaveProfile.setOnClickListener {
+                currentUser?.let { user ->
+                    val updatedPhone = binding.etPhone.text.toString()
+                    val updatedUser = user.copy(phone = updatedPhone, photoUrl = imageUri.toString())
+                    profileViewModel.updateUserProfile(updatedUser, imageUri!!)
+                } ?: Toast.makeText(context, "User data not available", Toast.LENGTH_SHORT).show()
+            }
+
+            profileViewModel.errorMessage.observe(viewLifecycleOwner) { error ->
+                error?.let { Toast.makeText(context, it, Toast.LENGTH_SHORT).show() }
+            }
+
+            profileViewModel.isProfileUpdated.observe(viewLifecycleOwner) { success ->
+                if (success) {
+                    currentUser?.let { user ->
+                        binding.tvPhone.text = user.phone
+                        binding.etPhone.setText(user.phone)
+                        FirebaseAuth.getInstance().currentUser?.uid?.let {
+                            profileViewModel.fetchUserDetails(user.userId)
+                        }
+                        isEditMode = false
+                        toggleEditMode(isEditMode)
+                    }
+                    Toast.makeText(context, "Profile updated successfully", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "Failed to update profile", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            FirebaseAuth.getInstance().currentUser?.uid?.let { profileViewModel.fetchUserDetails(it) }
+
+            val currentUserId = FirebaseAuth.getInstance().currentUser?.uid ?: return
+            postViewModel.fetchUserPosts(currentUserId)
+
+            val adapter = PostAdapter(
+                lifecycleOwner = viewLifecycleOwner,
+                context = context,
+                onEditClick = { post -> updatePost(post) },
+                onDeleteClick = { post -> deletePost(post) }
+            )
+            binding.recyclerViewUserPosts.adapter = adapter
+            binding.recyclerViewUserPosts.layoutManager = LinearLayoutManager(context)
+
+            postViewModel.userPosts.observe(viewLifecycleOwner) { posts ->
+                adapter.submitList(posts)
+            }
+
+            binding.btnSignOut.setOnClickListener {
+                FirebaseAuth.getInstance().signOut()
+                Toast.makeText(context, "Signed Out Successfully", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.signInFragment)
+            }
         }
     }
+
+    private val pickImage =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                imageUri = uri
+                val imageBtn: ImageButton = binding.btnProfileImage
+                imageUri?.let { profileViewModel.setImageUri(it) }
+                imageBtn.setImageURI(uri)
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.image_error),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 
     private val imagePickerLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
@@ -134,6 +239,22 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
                 commentViewModel.deleteComment(comment.commentId)
             }
             postViewModel.deletePost(post.postId)
+        }
+    }
+
+    private fun toggleEditMode(isEdit: Boolean) {
+        if (isEdit) {
+            binding.tvPhone.visibility = View.GONE
+            binding.etPhone.visibility = View.VISIBLE
+            binding.etPhone.isEnabled = true
+            binding.btnSaveProfile.visibility = View.VISIBLE
+            binding.btnProfileImage.isEnabled = true
+        } else {
+            binding.tvPhone.visibility = View.VISIBLE
+            binding.etPhone.visibility = View.GONE
+            binding.etPhone.isEnabled = false
+            binding.btnSaveProfile.visibility = View.GONE
+            binding.btnProfileImage.isEnabled = false
         }
     }
 

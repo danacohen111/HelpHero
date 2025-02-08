2cb9184c10dea0edfaa736be3a5eaac3f69490b8
Ilana Belokon <ilana.belokon2@cs.colman.ac.il>
2025-02-08 13:16:57 +0200

    implement edit mode in profile

--- app/src/main/java/com/example/helphero/ui/profile/profileFragment.kt
+++ app/src/main/java/com/example/helphero/ui/profile/profileFragment.kt
@@ -1,29 +1,53 @@
 package com.example.helphero.ui.profile
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.helphero.R
 import com.example.helphero.databases.users.UserDatabase
 import com.example.helphero.databinding.FragmentProfileBinding
+import com.example.helphero.models.User
 import com.example.helphero.repositories.UserRepository
 import com.example.helphero.ui.viewmodels.ProfileViewModel
 import com.example.helphero.ui.viewmodels.ProfileViewModelFactory
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
-import jp.wasabeef.picasso.transformations.CropCircleTransformation
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private lateinit var profileViewModel: ProfileViewModel
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private var isEditMode = false
+    private var currentUser: User? = null
+    private var imageUri: Uri? = null
+
+    private val pickImage =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                imageUri = uri
+                val imageBtn: ImageButton = binding.btnProfileImage
+                imageUri?.let { profileViewModel.setImageUri(it) }
+                imageBtn.setImageURI(uri)
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.image_error),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,19 +72,44 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
         // Observe user data and update UI
         profileViewModel.user.observe(viewLifecycleOwner) { user ->
+            currentUser = user
             binding.tvUsername.text = user.name
             binding.tvEmail.text = user.email
             binding.tvPhone.text = user.phone
+            binding.etPhone.setText(user.phone)
 
             if (!user.photoUrl.isNullOrEmpty()) {
                 Picasso.get()
                     .load(user.photoUrl)
+                    .fit()
                     .placeholder(R.drawable.ic_profile_placeholder)
-                    .transform(CropCircleTransformation())
-                    .into(binding.profileImageView)
+                    .into(binding.btnProfileImage)
             } else {
-                binding.profileImageView.setImageResource(R.drawable.ic_profile_placeholder)
+                binding.btnProfileImage.setImageResource(R.drawable.ic_profile_placeholder)
             }
+        }
+
+        binding.btnProfileImage.setOnClickListener {
+            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+
+        binding.ivEditProfile.setOnClickListener {
+            isEditMode = !isEditMode
+            toggleEditMode(isEditMode)
+        }
+
+        binding.btnSaveProfile.setOnClickListener {
+            currentUser?.let { user ->
+                val updatedPhone = binding.etPhone.text.toString()
+                val updatedUser = user.copy(phone = updatedPhone)
+
+                profileViewModel.updateUserProfile(updatedUser)
+                isEditMode = false
+                toggleEditMode(isEditMode)
+                Toast.makeText(requireContext(), "Profile updated successfully", Toast.LENGTH_SHORT).show()
+            } ?: Toast.makeText(requireContext(), "User data not available", Toast.LENGTH_SHORT)
+                .show()
         }
 
         // Observe error messages
@@ -76,6 +125,24 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
             FirebaseAuth.getInstance().signOut()
             Toast.makeText(requireContext(), "Signed Out Successfully", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.signInFragment)
+        }
+
+    }
+
+    private fun toggleEditMode(isEdit: Boolean) {
+        if (isEdit) {
+            binding.tvPhone.visibility = View.GONE
+            binding.etPhone.visibility = View.VISIBLE
+            binding.etPhone.isEnabled = true
+            binding.btnSaveProfile.visibility = View.VISIBLE
+            binding.btnProfileImage.isEnabled = true
+
+        } else {
+            binding.tvPhone.visibility = View.VISIBLE
+            binding.etPhone.visibility = View.GONE
+            binding.etPhone.isEnabled = false
+            binding.btnSaveProfile.visibility = View.GONE
+            binding.btnProfileImage.isEnabled = false
         }
     }
 
--- app/src/main/java/com/example/helphero/ui/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/com/example/helphero/ui/viewmodels/ProfileViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.helphero.ui.viewmodels
 
+import android.net.Uri
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.helphero.models.User
@@ -9,6 +11,12 @@ class ProfileViewModel(private val userRepository: UserRepository) : ViewModel()
     val user = MutableLiveData<User>()
     val errorMessage = MutableLiveData<String>()
     val isLoading = MutableLiveData<Boolean>()
+    val successMessage = MutableLiveData<String>()
+    private val _imageUri = MutableLiveData<Uri?>()
+
+    fun setImageUri(uri: Uri) {
+        _imageUri.value = uri
+    }
 
     fun fetchUserDetails(userId: String) {
         isLoading.value = true
@@ -21,6 +29,19 @@ class ProfileViewModel(private val userRepository: UserRepository) : ViewModel()
             errorMessage.value = error
             isLoading.value = false
         })
+    }
+
+    fun updateUserProfile(updatedUser: User) {
+        isLoading.value = true
+        userRepository.updateUser(updatedUser,
+            onSuccess = {
+                successMessage.value = "Profile updated successfully"
+                isLoading.value = false
+            },
+            onError = { error ->
+                errorMessage.value = error
+                isLoading.value = false
+            })
     }
 
 }
